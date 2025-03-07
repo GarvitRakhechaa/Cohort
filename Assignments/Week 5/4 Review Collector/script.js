66695e315f2b0f5ca6fce6b23adaee46aabf1a4d
Garvit Rakhecha <garvitrakhecha786@gmail.com>
2025-03-08 01:25:08 +0530

    review collector project done

---
+++ Assignments/Week 5/4 Review Collector/script.js
@@ -0,0 +1,68 @@
+let Submit = document.getElementById('Submit')
+let Name = document.getElementById('name')
+let title = document.getElementById('title')
+let review = document.getElementById('review')
+let reviewSection = document.getElementById('reviewSection')
+let imogies = ["😡", "😞", "😐", "🙂", "😃"]
+let mainImogies = document.querySelectorAll('.imogiees')
+let str = ""
+
+
+mainImogies.forEach(emogy => emogy.addEventListener("click", function () {
+        str = ""
+        mainImogies.forEach(e => e.classList.remove("circle"));
+        this.classList = "circle"
+        console.log(this.id)
+
+        for (i = 0; i < this.id; i++) {
+                str = `${str} ⭐`
+                console.log(str)
+        }
+}));
+Submit.addEventListener("click", updateReview)
+
+function updateReview() {
+        let now = new Date()
+        let TITLE = title.value
+        let REVIEW = review.value
+        let NAME = Name.value
+        let EMOGY = str
+
+        if (TITLE.trim() === "" || EMOGY.trim() === "" || REVIEW.trim() === "" || NAME.trim() === "") {
+                alert("All values should be filled");
+        }
+        else {
+                let ReviewInfo = document.createElement('div')
+                ReviewInfo.classList = "reviewInfo"
+                let h1 = document.createElement('h1')
+                let imogie = document.createElement('h1')
+                let p1 = document.createElement('p')
+                let h4 = document.createElement('h4')
+                let p2 = document.createElement('p')
+
+                h1.innerText = title.value
+                title.value = ""
+                console.log(TITLE)
+
+                imogie.innerText = str
+                str = ""
+                p1.innerText = review.value
+                review.value = ""
+                h4.innerText = Name.value
+                Name.value = ""
+                p2.innerText = `${now.toLocaleDateString()}  ${now.toLocaleTimeString()}`
+
+
+                ReviewInfo.appendChild(h1)
+                ReviewInfo.appendChild(imogie)
+                ReviewInfo.appendChild(p1)
+                ReviewInfo.appendChild(h4)
+                ReviewInfo.appendChild(p2)
+                console.log(ReviewInfo)
+                reviewSection.prepend(ReviewInfo)
+
+        }
+
+
+
+}
